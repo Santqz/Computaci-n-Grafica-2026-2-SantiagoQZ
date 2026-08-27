b1c98187d05d0ed49fb2dc9e5cca7403c2961823
Santqz <santiagoquintero703@gmail.com>
2026-08-27 11:08:28 -0500

    movimiento

--- src/js/mainClass3.js
+++ src/js/mainClass3.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -8,12 +9,26 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// Creation of a cube
+const geometry = new THREE.BoxGeometry( 1, 4, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ffff, wireframe: true } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+const controls = new OrbitControls( camera, renderer.domElement );
+camera.position.set( 0, -1.5, 9 );
+controls.update();
+
+// Grid  Helper
+const size = 10;
+const divisions = 10;
+const gridHelper = new THREE.GridHelper( size, divisions );
+scene.add( gridHelper );
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
+
 
 function animate( time ) {
 
