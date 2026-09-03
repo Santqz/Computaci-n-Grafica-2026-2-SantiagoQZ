788a5aa89a549ba12f014b2c3e1fa52448476eda
Santqz <santiagoquintero703@gmail.com>
2026-09-03 14:36:43 -0500

    CLASE 4 QUIZ PLANETAS

--- src/js/mainClass4.js
+++ src/js/mainClass4.js
@@ -57,64 +57,64 @@ const shapeData = [
     {
         name: 'Sol',
         geometry: new THREE.SphereGeometry(6, 50, 50),
-        color: 0xffd700,
+        color: 0xffff00,
         posX: 0
     },
 
     {
         name: 'Mercurio',
         geometry: new THREE.SphereGeometry(3.8, 40, 40),
-        color: 0x888888,
+        color: 0x555555,
         posX: 11.5
     },
 
     {
         name: 'Venus',
         geometry: new THREE.SphereGeometry(3.2, 40, 40),
-        color: 0xffa500,
+        color: 0xcc6600,
         posX: 20
     },
 
     {
         name: 'Tierra',
         geometry: new THREE.SphereGeometry(2.5, 35, 35),
-        color: 0x0000ff,
+        color: 0xfaf0e6,
         posX: 26.5
     },
 
     {
         name: 'Marte',
         geometry: new THREE.SphereGeometry(2, 32, 32),
-        color: 0xff0000,
-        posX: 32.5
+        color: 0xaa0000,
+        posX: 31.5
     },
 
     {
         name: 'Jupiter',
         geometry: new THREE.SphereGeometry(2, 30, 30),
-        color: 0xc28b62,
-        posX: 40.5
+        color: 0x996633,
+        posX: 36.5
     },
 
     {
         name: 'Saturno',
         geometry: new THREE.SphereGeometry(1.6, 25, 25),
-        color: 0xffd27f,
-        posX: 60
+        color: 0xccaa66,
+        posX: 42
     },
 
     {
         name: 'Urano',
         geometry: new THREE.SphereGeometry(1.2, 20, 20),
-        color: 0x66ffff,
-        posX: 70
+        color: 0x33cccc,
+        posX: 48
     },
 
     {
         name: 'Neptuno',
         geometry: new THREE.SphereGeometry(1, 15, 15),
-        color: 0x3333ff,
-        posX: 80
+        color: 0xc71585,
+        posX: 53
     }
 
 ];
@@ -126,10 +126,15 @@ const meshes = [];
 // PLANETAS
 
 shapeData.forEach((shapeData) => {
+    let material;
+    // SOL
+    if (shapeData.name === 'Sol') {material = new THREE.MeshBasicMaterial({color: 0xffff00});
 
-    const material = new THREE.MeshStandardMaterial({
-        color: shapeData.color
-    });
+    } else {
+
+        material = new THREE.MeshStandardMaterial({color: shapeData.color,roughness: 0.8,metalness: 0.1});
+
+    }
 
     const mesh = new THREE.Mesh(
         shapeData.geometry,
@@ -143,22 +148,78 @@ shapeData.forEach((shapeData) => {
 
 });
 
+// ANILLOS
 
-// ANILLO
+const ringMaterial = new THREE.MeshBasicMaterial({color: 0xb89b63,side: THREE.DoubleSide});
+const ring1 = new THREE.Mesh(new THREE.RingGeometry(1.5, 1.8, 64),ringMaterial);
+const ring2 = new THREE.Mesh(new THREE.RingGeometry(1.85, 2.2, 64),ringMaterial);
 
-const ringGeometry = new THREE.RingGeometry(1.5, 3, 32);
-const ringMaterial = new THREE.MeshBasicMaterial({color: 0xd2b48c,side: THREE.DoubleSide});
+ring1.rotation.x = Math.PI / 2;
+ring2.rotation.x = Math.PI / 2;
 
-const ring = new THREE.Mesh(ringGeometry,ringMaterial);
-ring.rotation.x = Math.PI / 2;
-ring.position.x = 45;
-scene.add(ring);
+ring1.position.x = 42;
+ring2.position.x = 42;
+
+scene.add(ring1);
+scene.add(ring2);
+
+// VELOCIDAD PLANETAS
+
+const velocidades = [
+    0,       // Sol
+    0.0008,  // Mercurio
+    0.0006,  // Venus
+    0.0005,  // Tierra
+    0.0004,  // Marte
+    0.0003,  // Jupiter
+    0.0002,  // Saturno
+    0.00015, // Urano
+    0.0001   // Neptuno
+];
+// ÁNGULO PLANETAS
+
+const angulos = [
+    0, // Sol
+    0, // Mercurio
+    0, // Venus
+    0, // Tierra
+    0, // Marte
+    0, // Jupiter
+    0, // Saturno
+    0, // Urano
+    0  // Neptuno
+];
 
 // ANIMACIÓN
 
-function animate() {
-
+function animate(time) {
     controls.update();
+
+    // MOVIMIENTO PLANETAS
+
+    meshes.forEach((mesh, index) => {
+
+        if (index === 0) return;
+        angulos[index] += velocidades[index] * 10;
+        const anguloDiscontinuo = Math.floor(angulos[index] * 20) / 20;
+
+        const distancia = shapeData[index].posX;
+        mesh.position.x = Math.cos(anguloDiscontinuo) * distancia;
+        mesh.position.z =  Math.sin(anguloDiscontinuo) * distancia;
+
+    });
+
+
+    // MOVER ANILLOS 
+
+    ring1.position.x = meshes[6].position.x;
+    ring1.position.z = meshes[6].position.z;
+
+    ring2.position.x = meshes[6].position.x;
+    ring2.position.z = meshes[6].position.z;
+
+    // MOSTRAR
+
     renderer.render(scene, camera);
 
 }
